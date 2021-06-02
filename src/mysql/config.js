e0242a59cee5ba95d0c7eca6afc2d628bafa186a
ethan.nguyen <ethan.nguyen@deliveree.com>
2021-06-02 20:34:28 +0700

    add config file for database

---
+++ src/mysql/config.js
@@ -0,0 +1,20 @@
+{
+    const config = {
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      // password: "011218",
+      database: "mydb",
+      // multipleStatements: true
+    };
+  /* User for AWS config S3
+    const accessKey = "xxxxxxxxxxxxxxxxxx";
+    const secretKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    const Bucket = "??????";
+    const ACL = "public-read-write";
+    const ContentType = "image/jpg";
+  */
+    module.exports = {
+      config,
+    };
+  }
