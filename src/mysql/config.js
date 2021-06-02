a8fa4f56cd4cebd6f2b4b7f51326ade5a4134868
ethan.nguyen <ethan.nguyen@deliveree.com>
2021-06-02 23:58:04 +0700

    edit config db to connect aws

--- src/mysql/config.js
+++ src/mysql/config.js
@@ -1,11 +1,16 @@
 {
     const config = {
-      host: "127.0.0.1",
+      // host: "127.0.0.1",
+      // port: 3306,
+      // user: "root",
+      // // password: "011218",
+      // database: "mydb",
+      // // multipleStatements: true
+      host: "moviedb.cs8d928m2ggm.us-east-2.rds.amazonaws.com",
+      user: "admin",
+      password: "Nguyen1997..",
       port: 3306,
-      user: "root",
-      // password: "011218",
-      database: "mydb",
-      // multipleStatements: true
+      database: "moviedb"
     };
   /* User for AWS config S3
     const accessKey = "xxxxxxxxxxxxxxxxxx";
