0135f190927252edf6bd577cddc454702926ed96
1660392hcmus <ntainguyenhcmus@gmail.com>
2021-06-04 11:41:35 +0700

    cors config

--- app.js
+++ app.js
@@ -74,9 +74,7 @@ App.use(
 App.use(cors());
 
 App.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://project-final-movie.herokuapp.com");
-  // res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
