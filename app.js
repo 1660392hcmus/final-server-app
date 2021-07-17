5bc7e6baa99e562a8b06c39d32e5617d780fbec9
NguyenTaiNguyen <nguyen.nguyentai@haravan.com>
2021-07-17 19:25:22 +0700

    change cors

--- app.js
+++ app.js
@@ -77,8 +77,8 @@ App.use(
 
 App.use(function(req, res, next) {
   // res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", 'http://project-final-movie.herokuapp.com');
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.setHeader("Access-Control-Allow-Origin", 'http://project-final-movie.herokuapp.com');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
